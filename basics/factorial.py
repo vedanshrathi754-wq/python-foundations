3fc9a12815bcac1254dd801082a8dcddb06b025d
vedanshrathi754-wq <vedanshrathi754@gmail.com>
2026-02-11 15:07:12 +0530

    Update factorial.py

--- basics/factorial.py
+++ basics/factorial.py
@@ -1,6 +1,13 @@
-n=int(input("Enter a Number:"))
-      fact=1
-for i in range (1,n+1):
-  fact=fact*i
-  print("factorial:",fact)
-  
+# Program to calculate factorial of a number
+# Factorial of n = n * (n-1) * (n-2) ... * 1
+
+n = int(input("Enter a number: "))
+
+fact = 1  # variable to store factorial value
+
+# Loop from 1 to n
+for i in range(1, n + 1):
+    fact = fact * i
+
+print("Factorial:", fact)
+
